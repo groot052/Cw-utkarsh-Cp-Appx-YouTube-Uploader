503d63f768a6f9a6ce105f6336783755f1d92fcb
groot052 <111137138+groot052@users.noreply.github.com>
2025-08-31 12:28:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7363610742:AAHRdYOwGwg63pc3xnnFh1ZsEDDsOdhTpOM")
-    API_ID = int(os.environ["API_ID", 21179966]
-    API_HASH = os.environ["API_HASH", "d97919fb0a3c725e8bb2a25bbb37d57c"]
-    AUTH_USERS = "7326397503"""
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8021957130:AAE-6wO_zO9YTeYj2BUNO2LoP7p_gDlV-YI")
+    API_ID = int(os.environ["API_ID", 20531922]
+    API_HASH = os.environ["API_HASH", "1281c125a3e908948a9a64a58099a2f3"]
+    AUTH_USERS = "5240924887"""
  
+
